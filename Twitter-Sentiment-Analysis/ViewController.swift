3eccf920e94d10acfc3724b198fa5c8fc286c04a
Elina <ardentlycurious101@gmail.com>
2019-08-08 11:21:20 -0700

    Initial commit.

---
+++ Twitter-Sentiment-Analysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter-Sentiment-Analysis
+//
+//  Created by Elina Lua Ming on 8/8/19.
+//  Copyright © 2019 Elina Lua Ming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
